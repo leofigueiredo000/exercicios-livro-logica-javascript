85e46d4dfae000a1e76d0f18eada81b41085418e
leot120 <leozim543@gmail.com>
2024-10-01 13:34:20 -0300

    terminando ex6 cap 3

--- cap3/js/ex3_5.js
+++ cap3/js/ex3_5.js
@@ -1,25 +1,23 @@
-function VerificarRaiz(){
-
+function calcularRaiz() {
   //referencias
   var inNumero = document.getElementById("inNumero");
   var outResposta = document.getElementById("outResposta");
-  var btExibirRaiz = document.getElementById("btExibirRaiz");
-
 
   //dados de entrada
   var numero = Number(inNumero.value);
-  var raiz  = Math.sqrt(numero);
+  
 
-  if(raiz == 0 || isNaN(raiz)){
-
+  if (raiz == 0 || isNaN(raiz)) {
     alert("Digite um número Válido...");
+    inNumero.focus();
+    return;
   }
-   
-  if(raiz === Math.floor(raiz) )
-  {
-outResposta.textContent = "Há raiz exata para o numero" 
-    
+  var raiz = Math.sqrt(numero);
+  if (raiz == Math.floor(raiz)) {
+    outResposta.textContent = "Raiz: " + raiz;
+  } else {
+    outResposta.textContent = "Não há raiz para o numero " + numero;
   }
-
-
 }
+var btExibir = document.getElementById("btExibir");
+addEventListener.btExibirRaiz("click", calcularRaiz);
