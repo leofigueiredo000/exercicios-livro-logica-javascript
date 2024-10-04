d5cfacca674fc9fda974abba0f423c73a704058b
leot120 <leozim543@gmail.com>
2024-10-04 08:23:53 -0300

    cap 3 concluido

--- cap3/js/resp3_d.js
+++ cap3/js/resp3_d.js
@@ -1,32 +1,35 @@
-function verificarLados(){
-//referencias
-var inLadoA = document.getElementById("inLadoA");
-var inladoB = document.getElementById("inLadoB");
-var inladoC = document.getElementById("inLadoC");
-var outValidacaoTriangulo = document.getElementById("outValidacaoTriangulo");
-var outTipoTriangulo = document.getElementById("outTipoTriangulo");
+function verificarLados() {
+  //referencias
+  var inLadoA = document.getElementById("inLadoA");
+  var inladoB = document.getElementById("inLadoB");
+  var inladoC = document.getElementById("inLadoC");
+  var outValidacaoTriangulo = document.getElementById("outValidacaoTriangulo");
+  var outTipoTriangulo = document.getElementById("outTipoTriangulo");
 
-//valores de entrada 
- var ladoA = Number(inLadoA.value);
- var ladoB = Number(inladoB.value);
- var ladoC = Number(inladoC.value);
+  //valores de entrada
+  var ladoA = Number(inLadoA.value);
+  var ladoB = Number(inladoB.value);
+  var ladoC = Number(inladoC.value);
 
- //dois lados somados não podem ser maior que o terceio (a+b<c)
+  //condição de existencia de um triangulo(a+b > c)
 
- if(ladoC <= ladoA + ladoB && ladoB <= ladoA + ladoC && ladoA <= ladoB + ladoC ){
+  if (
+    (ladoA + ladoB > ladoC) & (ladoB + ladoC > ladoA) &&
+    ladoA + ladoC > ladoB
+  ) {
+    outValidacaoTriangulo.textContent = "Lados podem formar um triângulo.";
 
-
-  outValidacaoTriangulo.textContent = "Lados podem formar um triângulo";
-
-}else{
-  outValidacaoTriangulo.textContent = "Lados não podem formar um Triângulo";
-}
- 
-
-
- 
-
-
+    if (ladoA == ladoB && ladoB == ladoC) {
+      outTipoTriangulo.textContent = "Triangulo Equilátero.";
+    } else if (ladoA == ladoB || ladoA == ladoC || ladoB == ladoC) {
+      outTipoTriangulo.textContent = "Triangulo isóceles";
+    } else {
+      outTipoTriangulo.textContent = "Triângulo escaleno";
+    }
+  } else {
+    outValidacaoTriangulo.textContent = "Lados não podem formar um triângulo.";
+    outTipoTriangulo.textContent = ""; // Limpa o tipo de triângulo
+  }
 }
 var btVerificarLados = document.getElementById("btVerificarLados");
-btVerificarLados.addEventListener("click",verificarLados);
+btVerificarLados.addEventListener("click", verificarLados);
