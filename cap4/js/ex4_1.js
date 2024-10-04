9bd4b975d85cb5d851a7b5af7cff1b01435f9a50
leot120 <leozim543@gmail.com>
2024-10-04 13:49:23 -0300

    inicio cap 4

---
+++ cap4/js/ex4_1.js
@@ -0,0 +1,16 @@
+function mostrarTabuada() {
+  var inNumero = document.getElementById("inNumero");
+  var outResposta = document.getElementById("outResposta");
+
+  var numero = Number(inNumero.value);
+
+  for (var i = 1; i <= 10; i++) {
+     
+    var resposta = resposta + numero + " x " + i + " = " + numero * i + "\n";
+    
+    outResposta.textContent = resposta
+  }
+  
+}
+var btMostrarTabuada = document.getElementById("btMostrarTabuada");
+  btMostrarTabuada.addEventListener("click", mostrarTabuada);
