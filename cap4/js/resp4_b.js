6dd606bd426e58921c0d2538ea04d921f51a7d53
Leo Figueiredo <leozim543@gmail.com>
2024-10-19 12:48:37 -0300

    ex b cap 4 concluido

---
+++ cap4/js/resp4_b.js
@@ -0,0 +1,29 @@
+function PreverChinchilas() {
+  //referencias
+  var inNumeroChinchilas = document.getElementById("inNumeroChinchilas");
+  var inNumeroAnos = document.getElementById("inNumeroAnos");
+  var outPrevisao = document.getElementById("outPrevisao");
+
+  //criação de variaveis
+  var numeroChinchilas = Number(inNumeroChinchilas.value);
+  var numeroAnos = Number(inNumeroAnos.value);
+  var previsaoChinchilas = 0;
+  var triplice = numeroChinchilas;
+
+  for (var i = 1; i <= numeroAnos; i++) {
+    if (i == 1) {
+      previsaoChinchilas = i + "º ano: " + numeroChinchilas + " Chinchilas \n";
+      outPrevisao.textContent = previsaoChinchilas;
+    }
+
+    if (i >= 2) {
+      triplice = triplice * 3;
+      previsaoChinchilas =
+        previsaoChinchilas + i + "º ano: " + triplice + " Chinchilas \n";
+      outPrevisao.textContent = previsaoChinchilas;
+    }
+  }
+}
+
+var btPrevisaoChinchilas = document.getElementById("btPrevisaoChinchilas");
+btPrevisaoChinchilas.addEventListener("click", PreverChinchilas);
