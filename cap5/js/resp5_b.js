4b9ff6e8d6b81088a2a96e294739bed4b2a9d362
leot120 <leozim543@gmail.com>
2024-11-25 22:48:44 -0300

    cap 5 exercicio c concluido

--- cap5/js/resp5_b.js
+++ cap5/js/resp5_b.js
@@ -1,37 +1,60 @@
-var numeros  = [];
-function adicionarNumero(){
+var numeros = [];
+function adicionarNumero() {
+  var inNumero = document.getElementById("inNumero");
 
- var inNumero = document.getElementById("inNumero");
+  var numero = inNumero.value;
 
-var numero = inNumero.value;
-
-numeros.push(numero);
-
-//Condição para evitar numeros repetidos
-if( numero == numeros[numeros.length-1]){
-
-    alert("Numero repetido, Digite outro");
-
-    numeros.pop();
+  //condicional de caixa de texto vazia
+  if (isNaN(numero) || numero == "") {
+    alert("Digite um Número válido");
+    inNumero.focus();
     return;
-}
-var lista = "";
+  }
 
-for(var i = 0; i < numeros.length; i++){
-lista += numeros[i];
-
-
-}
-if( numero == numeros[numeros.length-1]){
-
-    alert("Numero repetido, Digite outro");
-
-    numeros.pop();
+  //condicional caso tenha um numero repetido no array
+  if (numeros.includes(numero)) {
+    alert("Numero Repetido, digite outro.");
+    inNumero = "";
+    inNumero.focus();
     return;
-}
+  } else {
+    numeros.push(numero);
+  }
+  document.getElementById("outLista").textContent =
+    "Numeros: " + numeros.join(",");
 
+  //limpar os dados na caixa
+  inNumero.value = "";
+  inNumero.focus();
 }
 var btAdicionar = document.getElementById("btAdicionar");
 btAdicionar.addEventListener("click", adicionarNumero);
 
+function verificarOrdem() 
 
+{
+    var  outLista2 = document.getElementById("outLista2");
+  if (numeros.length == 0) {
+    alert("Nenhum Número na lista.");
+
+    return;
+  }
+
+  var copia = numeros.slice();
+
+  for( var i =0; i< numeros.length -1; i++){
+
+    if(numeros[i] > copia[i + 1]){
+outLista2.textContent = "Atenção... Numeros não estão em ordem crescente"
+outLista2.classList.add("fonteAzul")
+
+    }else{
+        outLista2.textContent = "Numeros estão em ordem crescente"
+        outLista2.classList.add("fonteAzul")
+
+    }
+    
+  }
+}
+var btVerificar = document.getElementById("btVerificar");
+btVerificar.addEventListener("click", verificarOrdem);
--- cap5/js/resp5_c.js
+++ cap5/js/resp5_c.js
@@ -1,50 +1,68 @@
 //variavel global para armazenar os dados de entrada
 var concurso = [];
 
-function adicionarCandidato(){
-var inCandidato  = document.getElementById("inCandidato");
-var inAcertos = document.getElementById("inAcertos");
+function adicionarCandidato() {
+  var inCandidato = document.getElementById("inCandidato");
+  var inAcertos = document.getElementById("inAcertos");
 
-//atribuição de variaveis
-var candidato = inCandidato.value;
-var acertos = Number(inAcertos.value);
+  //atribuição de variaveis
+  var candidato = inCandidato.value;
+  var acertos = Number(inAcertos.value);
 
-if(candidato == "" || isNaN(acertos)){
+  if (candidato == "" || isNaN(acertos)) {
     alert("Digite informações validas.");
-inCandidato.focus();
-return;
-}
+    inCandidato.focus();
+    return;
+  }
 
-concurso.push({candidato:candidato,acertos:acertos});
+  concurso.push({ candidato: candidato, acertos: acertos });
 
-
-listarCandidato();
-
+  listarCandidato();
 }
 var btAdicionar = document.getElementById("btAdicionar");
-btAdicionar.addEventListener("click",adicionarCandidato);
+btAdicionar.addEventListener("click", adicionarCandidato);
 
-
-function listarCandidato(){
-
-//condicional caso não tenha nenhum elemento no vetor
-    if(concurso.length == 0 ){
-
+function listarCandidato() {
+  //condicional caso não tenha nenhum elemento no vetor
+  if (concurso.length == 0) {
     alert("Lista vazia");
     inCandidato.focus();
     return;
-}
+  }
 
-var lista = "";
+  var lista = "";
 
-//loop para ler o conteudo do array
-for(var i =0; i < concurso.length; i++){
-
-    lista += concurso[i].candidato + " - " + concurso[i].acertos + " acertos \n";
-
-
-}
-document.getElementById("outLista").textContent = lista;
+  //loop para ler o conteudo do array
+  for (var i = 0; i < concurso.length; i++) {
+    lista +=
+      concurso[i].candidato + " - " + concurso[i].acertos + " acertos \n";
+  }
+  document.getElementById("outLista").textContent = lista;
 }
 btListar = document.getElementById("btListar");
-btListar.addEventListener("click",listarCandidato);
+btListar.addEventListener("click", listarCandidato);
+
+function aprovarCandidato() {
+  var numero = prompt("Numero de acertos para aprovação?");
+  var notaCorte = Number(numero);
+
+  var lista = "";
+  var aprovados = [];
+
+  for (var i = 0; i < concurso.length; i++) {
+    if (concurso[i].acertos >= notaCorte) {
+      aprovados.push(concurso[i]);
+    }
+  }
+
+  aprovados.sort((a, b) => b.acertos - a.acertos);
+  for (var i = 0; i < concurso.length; i++) {
+    lista +=
+      aprovados[i].candidato + " - " + aprovados[i].acertos + " acertos \n";
+  }
+
+  document.getElementById("outLista").textContent = lista;
+}
+
+var btAprovados = document.getElementById("btAprovados");
+btAprovados.addEventListener("click", aprovarCandidato);
