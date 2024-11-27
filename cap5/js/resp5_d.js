78f87ba30c5f17e170c086a88eba32d922a69ff3
leot120 <leozim543@gmail.com>
2024-11-27 08:10:54 -0300

    quase no fim cap 5

--- cap5/js/resp5_d.js
+++ cap5/js/resp5_d.js
@@ -16,8 +16,52 @@ function adicionarNoticias() {
 
   noticias.push(noticia);
 
-
-  document.getElementById("outLista").textContent = aux;
+  listarNoticia();
 }
 var btAdicionar = document.getElementById("btAdicionar");
-btAdicionar.addEventListener("click",adicionarNoticias);
+btAdicionar.addEventListener("click", adicionarNoticias);
+
+function listarNoticia() {
+  //verificar se o array está vazio
+  if (noticias.length == 0) {
+    alert("Não há noticias.");
+    return;
+  }
+
+  var lista = "--------------------------------------------------\n";
+
+  //for para ler a lista
+  for (var i = 0; i < noticias.length; i++) {
+    lista += i + 1 + "º) " + noticias[i] + "\n";
+  }
+
+  document.getElementById("outLista").textContent = lista;
+}
+
+function listarUltimasNoticias() {
+  //se não tiver noticias
+  if (noticias.length == 0) {
+    alert("Sem noticias disponiveis");
+    return;
+  }
+
+  var copia = noticias.slice();
+  //colocar o vetor em forma descrescente
+  copia.reverse();
+
+  var lista = "--------------------------------------------------\n";
+
+  var quantidade = prompt("Quantas notícias deseja visualizar?");
+  Number(quantidade);
+
+  //mostrar as ultimas noticias
+  for (var i = 0; i < quantidade; i++) {
+
+lista+= quantidade - i + "º) " + copia[i] + "\n";
+
+
+  }
+  document.getElementById("outLista").textContent = lista;
+}
+var btNoticias = document.getElementById("btNoticias");
+btNoticias.addEventListener("click", listarUltimasNoticias);
